3e5bc7f21579a2c89f9d1b139fb2fb5e8975338a
hunahappy <manduhone@gmail.com>
2026-05-31 00:41:20 +0900

    kubuntu 2604

--- src/flash.sh
+++ src/flash.sh
@@ -8,7 +8,7 @@ if [ $? -eq 0 ]; then
     echo "업로드 성공! 모니터링을 시작합니다..."
     # minicom을 사용한 모니터링 (115200 보드레이트 설정)
     # -D: 포트 지정, -b: 보드레이트, -C: 로그파일 생성(선택사항)
-    picocom -e q /dev/ttyACM0
+    picocom /dev/ttyACM0
 else
     echo "업로드 실패. 모니터링을 건너뜁니다."
 fi
